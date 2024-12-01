ebcce047afb64ac7dc7b71ff2fa2a73145b1d9d9
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-12-01 13:21:36 -0800

    Update alignment_track_main.sh
    added links to all genomes (still need to be renamed)

--- alignment_track_main.sh
+++ alignment_track_main.sh
@@ -36,23 +36,24 @@ process_reference_genome() {
     virus_name="$1"
     genome_url="$2"
     echo "Processing $virus_name reference genome..."
-    wget -q "$genome_url" -O "$WORKDIR/${virus_name}_genome.fna.gz"
+    wget "$genome_url" -O "$WORKDIR/${virus_name}_genome.fasta"
     check_error
 
-    echo "Unzipping $virus_name reference genome..."
-    gunzip -f "$WORKDIR/${virus_name}_genome.fna.gz"
-    check_error
+    # echo "Unzipping $virus_name reference genome..."
+    # gunzip -f "$WORKDIR/${virus_name}_genome.fna.gz"
+    # check_error
 
     echo "Indexing $virus_name reference genome..."
-    samtools faidx "$WORKDIR/${virus_name}_genome.fna"
+    samtools faidx "$WORKDIR/${virus_name}_genome.fasta"
     check_error
 
     echo "Adding $virus_name reference genome to JBrowse..."
-    jbrowse add-assembly "$WORKDIR/${virus_name}_genome.fna" --out "$APACHE_ROOT/jbrowse2" --load copy
+    jbrowse add-assembly "$WORKDIR/${virus_name}_genome.fasta" --out "$APACHE_ROOT/jbrowse2" --load copy
     check_error
 }
 
 # Function to process a reference annotation
+# needs to be fixed if we no longer recieve zipped files... also don't have links yet
 process_reference_annotation() {
     virus_name="$1"
     annotation_url="$2"
@@ -88,15 +89,15 @@ process_comparison_genome() {
     comparison_genome_url="$3"
 
     echo "Building Bowtie2 index for $reference_virus_name genome..."
-    bowtie2-build "$WORKDIR/${reference_virus_name}_genome.fna" "$WORKDIR/${reference_virus_name}_genome_index"
+    bowtie2-build "$WORKDIR/${reference_virus_name}_genome.fasta" "$WORKDIR/${reference_virus_name}_genome_index"
     check_error
 
     echo "Downloading $comparison_virus_name..."
-    wget -q "$comparison_genome_url" -O "$WORKDIR/${comparison_virus_name}.fa"
+    wget -q "$comparison_genome_url" -O "$WORKDIR/${comparison_virus_name}.fasta"
     check_error
 
     echo "Aligning $comparison_virus_name to $reference_virus_name reference genome..."
-    bowtie2 -x "$WORKDIR/${reference_virus_name}_genome_index" -f "$WORKDIR/${comparison_virus_name}.fa" -S "$WORKDIR/${comparison_virus_name}.sam"
+    bowtie2 -x "$WORKDIR/${reference_virus_name}_genome_index" -f "$WORKDIR/${comparison_virus_name}.fasta" -S "$WORKDIR/${comparison_virus_name}.sam"
     check_error
 
     echo "Converting $comparison_virus_name alignment to BAM format..."
@@ -119,59 +120,51 @@ process_comparison_genome() {
 ### Main Script Execution ###
 
 # Process DENV-1 Genomes
-process_reference_genome "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz"
-process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
+process_reference_genome "DENV-1, New Caledonia-2017-AVS-NC-094" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW315194.1&report=fasta&format=text"
+# process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
 
-process_comparison_genome "DENV-1" \
+process_comparison_genome "DENV-1, New Caledonia-2017-AVS-NC-094" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW315194.1&report=fasta&format=text" \
+    "DENV-1, Xishuangbanna Dai, China" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW386867.1&report=fasta&format=text"
+process_comparison_genome "DENV-1, New Caledonia-2017-AVS-NC-094" \
     "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_1_comparison1.fa"
-process_comparison_genome "DENV-1" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_1_comparison2.fa"
-process_comparison_genome "DENV-1" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_1_comparison3.fa"
+    "DENV-1, Guangzhou, China " "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=PQ357572.1&report=fasta&format=text"
 
 # Process DENV-2 Genomes
-process_reference_genome "DENV-2" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz"
-process_reference_annotation "DENV-2" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.gff.gz"
+process_reference_genome "DENV-2, Cosmopolitan" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW512496.1&report=fasta&format=text"
+# process_reference_annotation "DENV-2, Cosmopolitan" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.gff.gz"
 
-process_comparison_genome "DENV-2" \
+process_comparison_genome "DENV-2, Cosmopolitan" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW512496.1&report=fasta&format=text" \
+    "DENV-2, Malaysia" "https://example.com/DENV_2_comparison1.fa"
+process_comparison_genome "DENV-2, Cosmopolitan" \
     "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_2_comparison1.fa"
-process_comparison_genome "DENV-2" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_2_comparison2.fa"
-process_comparison_genome "DENV-2" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_2_comparison3.fa"
+    "DENV-2, Brazil" "https://example.com/DENV_2_comparison2.fa"
 
 # Process DENV-3 Genomes
-process_reference_genome "DENV-3" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz"
-process_reference_annotation "DENV-3" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.gff.gz"
+process_reference_genome "DENV-3, Florida" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821613.1&report=fasta&format=text"
+# process_reference_annotation "DENV-3" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.gff.gz"
 
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_3_comparison1.fa"
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_3_comparison2.fa"
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_3_comparison3.fa"
+process_comparison_genome "DENV-3, Florida" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821613.1&report=fasta&format=text" \
+    "DENV-3, V91A mutation" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821525.1&report=fasta&format=text"
+process_comparison_genome "DENV-3, Florida" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821613.1&report=fasta&format=text" \
+    "DENV-3, Senegal" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW288026.1&report=fasta&format=text"
 
 # Process DENV-4 Genomes
-process_reference_genome "DENV-4" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz"
-process_reference_annotation "DENV-4" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.gff.gz"
+process_reference_genome "DENV-4, Indonesia" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OL314742.1&report=fasta&format=text"
+# process_reference_annotation "DENV-4, Indonesia" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.gff.gz"
 
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_4_comparison1.fa"
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_4_comparison2.fa"
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_4_comparison3.fa"
+process_comparison_genome "DENV-4, Indonesia" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OL314742.1&report=fasta&format=text" \
+    "DENV-4, Paraguay" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OP811943.1&report=fasta&format=text"
+process_comparison_genome "DENV-4, Indonesia" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OL314742.1&report=fasta&format=text" \
+    "DENV-4, Cambodia 2010" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=KF543272.1&report=fasta&format=text"
+
+process_comparison_genome "DENV-4, Indonesia" \
+    "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OL314742.1&report=fasta&format=text" \
+    "DENV-4, French Polynesia 2009" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=JN832541.1&report=fasta&format=text"
 
 echo "All reference genomes, annotations, and comparison genomes have been processed and added to JBrowse."
