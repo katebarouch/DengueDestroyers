39e15a176353726bce54147e06e5fa379835eaab
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-12-01 15:12:31 -0800

    Update synteny_track_main.sh

--- synteny_track_main.sh
+++ synteny_track_main.sh
@@ -32,28 +32,26 @@ if [ ! -d "$APACHE_ROOT/jbrowse2" ]; then
 fi
 
 # Function to process a reference genome
-process_reference_genome() {
+process_genome() {
     virus_name="$1"
     genome_url="$2"
-    echo "Processing $virus_name reference genome..."
-    wget -q "$genome_url" -O "$WORKDIR/${virus_name}_genome.fna.gz"
-    check_error
 
-    echo "Unzipping $virus_name reference genome..."
-    gunzip -f "$WORKDIR/${virus_name}_genome.fna.gz"
+    echo "Processing $virus_name reference genome..."
+    wget "$genome_url" -O "$WORKDIR/${virus_name}_genome.fasta"
     check_error
 
     echo "Indexing $virus_name reference genome..."
-    samtools faidx "$WORKDIR/${virus_name}_genome.fna"
+    samtools faidx "$WORKDIR/${virus_name}_genome.fasta"
     check_error
 
     echo "Adding $virus_name reference genome to JBrowse..."
-    jbrowse add-assembly "$WORKDIR/${virus_name}_genome.fna" --out "$APACHE_ROOT/jbrowse2" --load copy
+    jbrowse add-assembly "$WORKDIR/${virus_name}_genome.fasta" --out "$APACHE_ROOT/jbrowse2" --load copy --name "$virus_name"
     check_error
 }
 
 # Function to process a reference annotation
-process_reference_annotation() {
+#needs to be fixed
+process_annotation() {
     virus_name="$1"
     annotation_url="$2"
     echo "Processing $virus_name annotations..."
@@ -81,71 +79,77 @@ process_reference_annotation() {
     check_error
 }
 
-# # Function to process comparison genome for synteny track
-# process_comparison_genome() {
-#     reference_virus_name="$1"
-#     comparison_virus_name="$2"
-#     comparison_genome_url="$3"
+# Function to process comparison genome for synteny track
+create_synteny_track() {
+    reference_virus_name="$1"
+    comparison_virus_name="$2"
 
-#    ...
-# }
+    echo "Creating synteny track between $reference_virus_name and $comparison_virus_name..."
+    minimap2 "$WORKDIR/${reference_virus_name}_genome.fasta" "$WORKDIR/${comparison_virus_name}_genome.fasta" > "$WORKDIR/${reference_virus_name}_vs_${comparison_virus_name}.paf"
+    check_error
+
+    echo "Adding synteny track to JBrowse..."
+    jbrowse add-track "$WORKDIR/${reference_virus_name}_vs_${comparison_virus_name}.paf" \
+        --assemblyNames "$reference_virus_name,$comparison_virus_name" \
+        --load copy \
+        --out "$APACHE_ROOT/jbrowse2"
+    check_error
+}
 
 ### Main Script Execution ###
 
 # Process DENV-1 Genomes
-process_reference_genome "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz"
-process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
+process_genome "DENV-1_New_Caledonia-2017-AVS-NC-094" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW315194.1&report=fasta&format=text" 
+process_genome "DENV-1_Xishuangbanna_Dai_China" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW386867.1&report=fasta&format=text" 
+process_genome "DENV-1_Guangzhou_China" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=PQ357572.1&report=fasta&format=text" 
 
-process_comparison_genome "DENV-1" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_1_comparison1.fa"
-process_comparison_genome "DENV-1" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_1_comparison2.fa"
-process_comparison_genome "DENV-1" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_1_comparison3.fa"
+
+create_synteny_track "DENV-1_New_Caledonia-2017-AVS-NC-094" \
+    "DENV-1_Xishuangbanna_Dai_China" 
+create_synteny_track "DENV-1_Xishuangbanna_Dai_China" \
+    "DENV-1_Guangzhou_China" 
+create_synteny_track "DENV-1_Guangzhou_China" \
+    "DENV-1_New_Caledonia-2017-AVS-NC-094" 
 
 # Process DENV-2 Genomes
-process_reference_genome "DENV-2" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz"
-process_reference_annotation "DENV-2" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.gff.gz"
+process_genome "DENV-2_Cosmopolitan" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW512496.1&report=fasta&format=text" 
+process_genome "DENV-2_Malaysia" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=PQ465587.1&report=fasta&format=text" 
+process_genome "DENV-2_Brazil" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=PP546320.1&report=fasta&format=text" 
 
-process_comparison_genome "DENV-2" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_2_comparison1.fa"
-process_comparison_genome "DENV-2" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_2_comparison2.fa"
-process_comparison_genome "DENV-2" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/871/845/GCF_000871845.1_ViralProj20183/GCF_000871845.1_ViralProj20183_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_2_comparison3.fa"
+create_synteny_track "DENV-2_Cosmopolitan" \
+    "DENV-2_Malaysia" 
+create_synteny_track "DENV-2_Cosmopolitan" \
+    "DENV-2_Brazil" 
+create_synteny_track "DENV-2_Malaysia" \
+    "DENV-2_Brazil" 
 
 # Process DENV-3 Genomes
-process_reference_genome "DENV-3" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz"
-process_reference_annotation "DENV-3" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.gff.gz"
+process_genome "DENV-3_Florida" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821613.1&report=fasta&format=text" 
+process_genome "DENV-3_V91A_mutation" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OQ821525.1&report=fasta&format=text" 
+process_genome "DENV-3_Senegal" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW288026.1&report=fasta&format=text" 
 
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_3_comparison1.fa"
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_3_comparison2.fa"
-process_comparison_genome "DENV-3" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788295.1_ASM478829v1/GCF_004788295.1_ASM478829v1_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_3_comparison3.fa"
+create_synteny_track "DENV-3_Florida" \
+    "DENV-3_V91A_mutation" 
+create_synteny_track "DENV-3_Florida" \
+    "DENV-3_Senegal" 
+create_synteny_track "DENV-3_V91A_mutation" \
+    "DENV-3_Senegal" 
 
 # Process DENV-4 Genomes
-process_reference_genome "DENV-4" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz"
-process_reference_annotation "DENV-4" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.gff.gz"
+process_genome "DENV-4_Indonesia" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OL314742.1&report=fasta&format=text" 
+process_genome "DENV-4_Paraguay" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OP811943.1&report=fasta&format=text" 
+process_genome "DENV-4_Cambodia_2010" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=KF543272.1&report=fasta&format=text" 
+process_genome "DENV-4_French_Polynesia_2009" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=JN832541.1&report=fasta&format=text" 
 
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant1" "https://example.com/DENV_4_comparison1.fa"
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant2" "https://example.com/DENV_4_comparison2.fa"
-process_comparison_genome "DENV-4" \
-    "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/004/788/295/GCF_004788296.1_ASM478830v1/GCF_004788296.1_ASM478830v1_genomic.fna.gz" \
-    "Variant3" "https://example.com/DENV_4_comparison3.fa"
+create_synteny_track "DENV-4_Indonesia" \
+    "DENV-4_Paraguay" 
+create_synteny_track "DENV-4_Indonesia" \
+    "DENV-4_Cambodia_2010" 
+create_synteny_track "DENV-4_Indonesia" \
+    "DENV-4_French_Polynesia_2009" 
+create_synteny_track "DENV-4_Paraguay" \
+    "DENV-4_Cambodia_2010" 
+create_synteny_track "DENV-4_Cambodia_2010" \
+    "DENV-4_French_Polynesia_2009" 
 
 echo "All reference genomes, annotations, and comparison genomes have been processed and added to JBrowse."
